d8bda7e1b78fff0f89b19a90bb84e2ed97296db4
Zobweyt <bsg@LAPTOP-92L378M9>
2022-08-18 16:41:31 +0300

    Safer handling of interactions

--- Extensions/InteractionExtensions.cs
+++ Extensions/InteractionExtensions.cs
@@ -4,22 +4,27 @@ namespace Gudgeon;
 
 internal static class InteractionExtensions
 {
-    public static async Task RespondWithStyleAsync(this IDiscordInteraction? interaction, EmbedStyle style, string message, bool autoDelete = false)
+    private delegate Task Respond(IDiscordInteraction interaction, Embed embed);
+    private static async Task RespondAsync(Respond respond, IDiscordInteraction interaction, EmbedStyle style, string? message)
     {
-        await interaction.RespondAsync(embed: style.BuildStyledEmbedMessage(message));
+        var embed = new EmbedBuilder()
+            .WithStyle(style)
+            .WithDescription(message ?? string.Empty)
+            .Build();
 
-        if (autoDelete)
-            await DelayedDeleteResponseAsync(interaction);
+        await respond(interaction, embed);
     }
-    public static async Task ModifyWithStyleAsync(this IDiscordInteraction? interaction, EmbedStyle style, string message, bool autoDelete = false)
+    private static async Task SendStyleAsync(IDiscordInteraction interaction, Embed embed)
+        => await interaction.RespondAsync(embed: embed);
+    private static async Task ModifyStyleAsync(IDiscordInteraction interaction, Embed embed)
     {
         var response = await interaction.GetOriginalResponseAsync();
         if (response == null)
             return;
 
         void AddProperties(MessageProperties x)
-        {                
-            x.Embed = style.BuildStyledEmbedMessage(message);
+        {
+            x.Embed = embed;
             x.Components = new ComponentBuilder().Build();
         }
 
@@ -27,13 +32,15 @@ internal static class InteractionExtensions
             await component.UpdateAsync(AddProperties);
         else
             await interaction.ModifyOriginalResponseAsync(AddProperties);
-
-        if (autoDelete)
-            await DelayedDeleteResponseAsync(interaction);
     }
-    public static async Task DelayedDeleteResponseAsync(this IDiscordInteraction interaction)
+
+    public static async Task RespondWithStyleAsync(this IDiscordInteraction interaction, EmbedStyle style, string? message)
+        => await RespondAsync(SendStyleAsync, interaction, style, message);
+    public static async Task ModifyWithStyleAsync(this IDiscordInteraction interaction, EmbedStyle style, string? message)
+        => await RespondAsync(ModifyStyleAsync, interaction, style, message);
+    public static async Task DelayedDeleteResponseAsync(this IDiscordInteraction interaction, TimeSpan span)
     {
-        await Task.Delay(TimeSpan.FromSeconds(8));
+        await Task.Delay(span);
         var response = await interaction.GetOriginalResponseAsync();
 
         if (response != null)
