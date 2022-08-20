1a614d280b5fb69075e000ef69420dd14a5bcad5
Zobweyt <bsg@LAPTOP-92L378M9>
2022-08-20 15:15:21 +0300

    Added `Styles`

---
+++ Utilities/Styles/SuccessStyle.cs
@@ -0,0 +1,11 @@
+﻿namespace Gudgeon;
+
+internal class SuccessStyle : EmbedStyle
+{
+    public override void Apply(string? name = null)
+    {
+        Name = name ?? "Success";
+        IconUrl = Icons.Success;
+        Color = Colors.Success;
+    }
+}
