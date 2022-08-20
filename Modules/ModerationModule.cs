afef9c395e468961c8bd0fe7656e10b531101522
Zobweyt <bsg@LAPTOP-92L378M9>
2022-08-20 15:17:25 +0300

    Refactoring

--- Modules/ModerationModule.cs
+++ Modules/ModerationModule.cs
@@ -7,29 +7,34 @@ namespace Gudgeon.Modules;
 [RequireUserPermission(GuildPermission.Administrator)]
 public class ModerationModule : GudgeonModuleBase
 {
+    public static int MaxBanReasonLenght { get { return 512; } }
+
     [RequireBotPermission(GuildPermission.BanMembers)]
-    [SlashCommand("ban", "Bans a member")]
+    [SlashCommand("ban", "Bans a user")]
     public async Task<RuntimeResult> BanAsync(
-        [Summary("user", "The user to be banned")] SocketGuildUser user,
+        [Summary("user", "The user to be banned")] IUser target,
         [Summary("reason", "The reason of banning")] string? reason = null)
     {
-        if (user.IsHierarchyHigher())
-            return GudgeonResult.FromError($"{user.Mention} hierarchy is higher than bot's.");
+        if (target is SocketGuildUser guildUser && guildUser.HasHigherHierarchy(true, Context.Interaction))
+            return GudgeonResult.FromSuccess();
 
-        await user.BanAsync(reason: reason);
-        return GudgeonResult.FromSuccess($"{user.Username}#{user.Discriminator} has been banned.");
+        if (reason?.Length > MaxBanReasonLenght)
+            return GudgeonResult.FromError($"Ban reason length should be less or equal **{MaxBanReasonLenght}**.");
+
+        await Context.Guild.AddBanAsync(target, reason: reason);
+        return GudgeonResult.FromSuccess($"{target.Username}#{target.Discriminator} has been banned.");
     }
 
     [RequireBotPermission(GuildPermission.ModerateMembers)]
-    [SlashCommand("timeout", "Timeouts a member")]
+    [SlashCommand("timeout", "Timeouts a guild user")]
     public async Task<RuntimeResult> TimeoutAsync(
-        [Summary("user", "The user to be timed out")] SocketGuildUser user,
-        [Summary("span", "The span of timeout (30s, 15m, 2d)")] TimeSpan span)
+        [Summary("user", "The user to be timed out")] IGuildUser user,
+        [Summary("span", "The span of timeout (ex. 12m, 32s)")] TimeSpan span)
     {
-        if (user.IsHierarchyHigher())
-            return GudgeonResult.FromError($"{user.Mention} hierarchy is higher than bot's.");
+        if (user.HasHigherHierarchy(true, Context.Interaction))
+            return GudgeonResult.FromSuccess();
         
         await user.SetTimeOutAsync(span);
-        return GudgeonResult.FromSuccess($"{user.Mention} has been timed out until <t:{(DateTimeOffset.Now + span).ToUnixTimeSeconds()}:t>.");
+        return GudgeonResult.FromSuccess($"{user.Username}#{user.Discriminator} has been timed out until <t:{(DateTimeOffset.Now + span).ToUnixTimeSeconds()}:t>.");
     }
 }
