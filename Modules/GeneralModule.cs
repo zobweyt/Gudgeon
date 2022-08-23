4bc2eb3442b4a4a81d58976e8c06ec3b8e25ca58
Zobweyt <bsg@LAPTOP-92L378M9>
2022-08-23 16:42:31 +0300

    Refactoring

---
+++ Modules/GeneralModule.cs
@@ -0,0 +1,36 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+
+namespace Gudgeon.Modules;
+
+public class GeneralModule : GudgeonModuleBase
+{
+    [SlashCommand("whois", "Get information about guild member")]
+    public async Task WhoisAsync(IGuildUser? user = null)
+    {
+        user ??= Context.User as IGuildUser;
+
+        var embed = new GudgeonEmbedBuilder()
+            .WithTitle($"{user.Username}#{user.Discriminator}")
+            .AddField("Joined", $"<t:{user.JoinedAt.Value.ToUnixTimeSeconds()}:F>")
+            .AddField("Created", $"<t:{user.CreatedAt.ToUnixTimeSeconds()}:F>")
+            .WithThumbnailUrl(user.GetAvatarUrl() ?? user.GetDefaultAvatarUrl())
+            .Build();
+
+        await RespondAsync(embed: embed);
+    }
+
+    [SlashCommand("avatar", "Get a user avatar")]
+    public async Task AvatarAsync(IGuildUser? user = null)
+    {
+        user ??= Context.User as IGuildUser;
+
+        var embed = new GudgeonEmbedBuilder()
+            .WithTitle($"{user.Username}#{user.Discriminator}")
+            .WithImageUrl((user.GetAvatarUrl() ?? user.GetDefaultAvatarUrl()).Replace("128", "4096"))
+            .Build();
+
+        await RespondAsync(embed: embed);
+    }
+}
