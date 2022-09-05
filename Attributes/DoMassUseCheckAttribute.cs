841ec7fdf58f6e9e64ef3ae2d2b9e5a54da43cf8
Никита Газиев <zobweyt@gmail.com>
2022-09-05 16:24:10 +0300

    Moved to main directory

---
+++ Attributes/DoMassUseCheckAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Discord;
+using Discord.Interactions;
+using System.Collections.Concurrent;
+
+namespace Gudgeon;
+
+public sealed class DoMassUseCheck : PreconditionAttribute
+{
+    public static readonly ConcurrentDictionary<ulong, string> Limits = new();
+
+    public override Task<PreconditionResult> CheckRequirementsAsync(IInteractionContext context, ICommandInfo command, IServiceProvider services)
+    {
+        string contextId = command.Module.Name + "//" + command.MethodName + "//" + command.Name;
+        ulong id = context.Guild?.Id ?? context.User.Id;
+
+        if (Limits.Any(x => x.Key == id && x.Value == contextId))
+            return Task.FromResult(PreconditionResult.FromError("The command is already running in this guild."));
+
+        Limits.GetOrAdd(id, contextId);
+        return Task.FromResult(PreconditionResult.FromSuccess());
+    }
+}
---
+++ Attributes/RateLimit/RateLimitItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Gudgeon;
+
+public class RateLimitItem
+{
+    public string? ContextId { get; init; }
+    public DateTime ExpireAt { get; init; }
+    public RateLimitItem(string? contextId, DateTime expireAt)
+    {
+        ContextId = contextId;
+        ExpireAt = expireAt;
+    }
+}
