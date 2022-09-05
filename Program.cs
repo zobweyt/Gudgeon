0032c7f6eed2b9af45b6b06fdff6a53a0b7c3f68
Никита Газиев <zobweyt@gmail.com>
2022-09-05 16:27:17 +0300

    Added Fergun.Interactive

--- Program.cs
+++ Program.cs
@@ -1,43 +1,33 @@
 ﻿using Discord;
 using Discord.Addons.Hosting;
 using Discord.WebSocket;
+using Fergun.Interactive;
 using Gudgeon.Services;
 
-namespace Gudgeon;
-
-internal class Program
-{
-    private static async Task Main()
+var host = Host.CreateDefaultBuilder()
+    .ConfigureDiscordHost((context, config) =>
     {
-        var host = Host.CreateDefaultBuilder()
-            .ConfigureDiscordHost((context, config) =>
-            {
-                config.SocketConfig = new DiscordSocketConfig
-                {
-                    LogLevel = LogSeverity.Debug,
-                    AlwaysDownloadUsers = true,
-                    MessageCacheSize = 200,
-                    GatewayIntents = GatewayIntents.All
-                };
+        config.SocketConfig = new DiscordSocketConfig
+        {
+            LogLevel = LogSeverity.Debug,
+            AlwaysDownloadUsers = true,
+            GatewayIntents = GatewayIntents.All,
+            MessageCacheSize = 200
+        };
+    
+        config.Token = context.Configuration["Token"];
+    })
+    .UseInteractionService((context, config) =>
+    {
+        config.LogLevel = LogSeverity.Debug;
+        config.UseCompiledLambda = true;
+    })
+    .ConfigureServices((context, services) =>
+    {
+        services
+        .AddHostedService<InteractionHandler>()
+        .AddSingleton<InteractiveService>();
+    })
+    .Build();
 
-                string? token = context.Configuration["Token"];
-
-                if (string.IsNullOrEmpty(token))
-                    throw new ArgumentException("The token is null or empty", nameof(token));
-
-                config.Token = token;
-            })
-            .UseInteractionService((context, config) =>
-            {
-                config.LogLevel = LogSeverity.Debug;
-                config.UseCompiledLambda = true;
-            })
-            .ConfigureServices((context, services) =>
-            {
-                services.AddHostedService<InteractionHandler>();
-            })
-            .Build();
-
-        await host.RunAsync();
-    }
-}
+await host.RunAsync();
