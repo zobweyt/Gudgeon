47c172490293ac3980c0501b0e2fe8af4dfc383f
Никита Газиев <zobweyt@gmail.com>
2022-09-05 16:25:55 +0300

    Removed extension methods

---
+++ Common/Entities/GudgeonModuleBase.cs
@@ -0,0 +1,12 @@
+﻿using Discord;
+using Discord.Interactions;
+
+namespace Gudgeon;
+
+[RequireContext(ContextType.Guild)]
+[RequireBotPermission(ChannelPermission.ViewChannel)]
+[RequireBotPermission(ChannelPermission.ReadMessageHistory)]
+[RequireBotPermission(ChannelPermission.SendMessages)]
+public abstract class GudgeonModuleBase : InteractionModuleBase<SocketInteractionContext>
+{
+}
