5ccbc052c4dba613b8c567759078a925c464c74e
Никита Газиев <zobweyt@gmail.com>
2022-09-08 23:43:50 +0300

    Moved to Gudgeon directory

---
+++ Gudgeon/Attributes/DisableConcurrentExecutionAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Discord;
+using Discord.Interactions;
+using System.Collections.Concurrent;
+
+namespace Gudgeon;
+
+public sealed class DisableConcurrentExecutionAttribute : PreconditionAttribute
+{
+    public static readonly ConcurrentDictionary<ulong, string> Items = new();
+
+    public override Task<PreconditionResult> CheckRequirementsAsync(IInteractionContext context, ICommandInfo commandInfo, IServiceProvider services)
+    {
+        ulong id = context.Guild?.Id ?? context.User.Id;
+        string contextId = commandInfo.Module.Name + "//" + commandInfo.MethodName + "//" + commandInfo.Name;
+
+        if (Items.Any(x => x.Key == id && x.Value == contextId))
+            return Task.FromResult(PreconditionResult.FromError("The command is already running."));
+
+        Items.GetOrAdd(id, contextId);
+        return Task.FromResult(PreconditionResult.FromSuccess());
+    }
+}
