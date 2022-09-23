be5babee8e89c055118f56763d21d4757390227b
Никита Газиев <zobweyt@gmail.com>
2022-09-23 20:20:51 +0300

    Add `ModuleBase` for games

---
+++ Gudgeon/Common/Entities/GameModuleBase.cs
@@ -0,0 +1,14 @@
+﻿using Discord;
+using Discord.Interactions;
+using Fergun.Interactive;
+
+namespace Gudgeon;
+
+[RequireBotPermission(ChannelPermission.UseExternalEmojis)]
+public abstract class GameModuleBase : GudgeonModuleBase
+{
+    protected GameModuleBase(InteractiveService interactive) 
+        : base(interactive)
+    {
+    }
+}
