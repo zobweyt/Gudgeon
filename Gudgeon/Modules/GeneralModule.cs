d5fb1f4f5b2cefb77d538b8e5089bd24b87e9121
zobweyt <zobweyt@gmail.com>
2022-12-24 16:00:22 +0300

    Remove outdated command.

--- Gudgeon/Modules/GeneralModule.cs
+++ Gudgeon/Modules/GeneralModule.cs
@@ -23,22 +23,6 @@ public class GeneralModule : GudgeonModuleBase
         await RespondAsync(embed: embed);
     }
 
-    [SlashCommand("whois", "Get information about guild member")]
-    public async Task WhoisAsync(IGuildUser? user = null)
-    {
-        user ??= Context.User as IGuildUser;
-
-        var embed = new EmbedBuilder()
-            .WithTitle($"{user.Username}#{user.Discriminator}")
-            .AddField("Joined", $"<t:{user.JoinedAt.Value.ToUnixTimeSeconds()}:R>")
-            .AddField("Created", $"<t:{user.CreatedAt.ToUnixTimeSeconds()}:R>")
-            .WithThumbnailUrl(user.GetAvatarUrl() ?? user.GetDefaultAvatarUrl())
-            .WithColor(Colors.Primary)
-            .Build();
-
-        await RespondAsync(embed: embed);
-    }
-
     [SlashCommand("avatar", "Get a user avatar")]
     public async Task AvatarAsync(IUser? user = null)
     {
