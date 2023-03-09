2f73a09789d6ef485f69a94eeda993b1389fbd13
zobweyt <zobweyt@gmail.com>
2023-03-10 02:02:31 +0300

    improved end game reasons responses

--- Gudgeon/Modules/Fun/Tiles/TilesModule.cs
+++ Gudgeon/Modules/Fun/Tiles/TilesModule.cs
@@ -11,47 +11,62 @@ public class TilesModule : GameModuleBase
     {
     }
 
-    [SlashCommand("2048", "The classic 2048 game", runMode: RunMode.Async)]
-    public async Task TilesAsync(
-        [Summary("size", "The size of the board")][Choice("4x4", 4), Choice("6x6", 6)] int boardSize = 4)
+    [SlashCommand("2048", "Built-in implementation of the classic 2048 game in discord", runMode: RunMode.Async)]
+    public async Task TilesAsync([Summary("size", "The size of the board")][Choice("4x4", 4), Choice("6x6", 6)] int maxBoardSize = 4)
+    {
+        string endReason = await StartGameAsync(maxBoardSize);
+        
+        IUserMessage? response = await GetOriginalResponseAsync();
+
+        if (response != null)
+        {
+            await response.ModifyAsync(message => message.Components = new ComponentBuilder().Build());
+        }
+
+        await FollowupAsync(endReason);
+    }
+
+    public async Task<string> StartGameAsync(int maxBoardSize)
     {
         Player player = new(Context.User);
-        TilesCore game = new(player, boardSize);
-        BoardConverter converter = new();
+        TilesCore game = new(player, maxBoardSize);
 
-        string guide = $"{player.User.Mention}, this is 2048 game in discord.\n" +
-                       "Click the buttons for the direction you want to shift the board.\n" +
-                       "Tiles with the same value will join together.\n" +
-                       "Try to reach the highest 2048 tile!";
-
-        await Context.Interaction.RespondWithBoard(converter.ConvertToString(game, guide));
+        await Context.Interaction.RespondAsync(game.ToDiscordMessageContent(), components: new BoardControlsBuilder().Build());
+        IUserMessage? response = await GetOriginalResponseAsync();
 
         while (true)
         {
-            IUserMessage? response = await GetOriginalResponseAsync();
-
-            var result = await _interactive.NextMessageComponentAsync(x =>
-                x.Message.Id == response.Id &&
-                x.User.Id == Context.Interaction.User.Id,
-                timeout: TimeSpan.FromMinutes(5));
+            var result = await _interactive.NextMessageComponentAsync(interaction =>
+                interaction.Message.Id == response.Id &&
+                interaction.User.Id == Context.Interaction.User.Id,
+                timeout: TimeSpan.FromMinutes(3)
+            );
 
             if (result.IsTimeout)
             {
-                await DeleteOriginalResponseAsync();
-                break;                
+                return $":hourglass: {player.User.Mention}, timeout! you have been inactive for too long.";
             }
 
             Direction direction = CustomIDs.Controls.First(x => x.Value == result.Value.Data.CustomId).Key;
-            game.TryMoveBoard(direction);
+            bool shifted = game.TryMoveBoard(direction);
 
-            if (game.HasMaxTile() || !game.CanShiftBoard())
+            if (shifted == false)
             {
-                string footer = $"{player.User.Mention}, you have ended the game with **{player.Score}** points!";
-                await result.Value.RespondWithBoard(converter.ConvertToString(game, footer));
-                break;
+                await result.Value.RespondAsync($":ping_pong: {player.User.Mention}, it's not possible to shift the board {direction.ToString().ToLower()}!", ephemeral: true);
+                continue;
             }
 
-            await result.Value.RespondWithBoard(converter.ConvertToString(game, $"{player.User.Mention} | score: **{player.Score}**"));
+            await result.Value.UpdateAsync(message => message.Content = game.ToDiscordMessageContent());
+
+            if (game.HasMaxTile())
+            {
+                return $":tada: {player.User.Mention}, congratulations! you have won the game!";
+            }
+
+            if (game.CanShiftBoard() == false)
+            {
+                return $":jigsaw: {player.User.Mention}, you have lost. hope luck will be on your side next time!";
+            }
         }
     }
 }
